109fed94f30ad4a5bb2cddf337684d23aae0d727
Paul M Edwards <pauledwards@gmail.com>
2020-09-11 22:03:39 -0700

    ex4 done

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,9 +1,35 @@
+debug = False
+
 def has_negatives(a):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    result = []
+    pos_data = {}
+    neg_data = {}
 
+    if debug and len(a) < 30: print(f"\ninput\n{a}")
+
+    for v in a:
+        if v > 0:
+            pos_data[v] = v
+        elif v < 0:
+            neg_data[abs(v)] = v
+
+    if debug: print(f"pos_data\t{pos_data}\nneg_data\t{neg_data}")
+
+    for k,v in neg_data.items():
+        test = 0
+        try:
+            test = pos_data[k]
+            if debug: print(f"k={k}, v={v}, test={test}")
+            if test > 0:
+                result.append(test)
+                if debug: print(f"appended '{test}' to result: {result}")
+        except:
+            pass
+
+    if debug: print(f"final result\n{result}")
     return result
 
 

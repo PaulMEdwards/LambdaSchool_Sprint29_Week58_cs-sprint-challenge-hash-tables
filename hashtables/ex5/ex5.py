40b1df8088ef0f973e7f65a95dc7731e592bb696
Paul M Edwards <pauledwards@gmail.com>
2020-09-14 19:45:57 -0700

    ex5 works on test_small, but not test_large

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -1,13 +1,59 @@
-# Your code here
+debug = True
 
-
+from hashtable import HashTable
 
 def finder(files, queries):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    result = []
+    lf = len(files)
+    lq = len(queries)
+    if debug:
+        print(f"#  files={lf}")
+        print(f"#queries={lq}")
+    data = HashTable(lq)
 
+    if debug: print() # blank line
+
+    fd = 'files.txt'
+    with open (fd, 'w') as filesOutput:
+        filesOutput.truncate(0)
+        for f in files:
+            t = f.split('/')
+            last = t[len(t)-1]
+            data.put(last, f)
+            if debug: filesOutput.write(f"{f}\n")
+    if debug: print(f"Wrote files data to '{fd}'...")
+    filesOutput.close()
+
+    fq = 'queries.txt'
+    with open (fq, 'w') as queriesOutput:
+        queriesOutput.truncate(0)
+        for q in queries:
+            if debug: queriesOutput.write(f"{q}\n")
+            test = None
+            try:
+                test = data.get(q)
+                if debug and test is not None: print(f"query: {q}, result: {test}")
+            except:
+                pass
+            if test is not None:
+                result.append(test)
+    if debug: print(f"Wrote queries data to '{fq}'...")
+    queriesOutput.close()
+
+    if debug:
+        fh = 'ht.txt'
+        with open (fh, 'w') as filesHashTable:
+            filesHashTable.truncate(0)
+            print(data, file=filesHashTable)
+        filesHashTable.close()
+        print(f"Wrote HashTable data to '{fh}'...")
+
+    if debug: print() # blank line
+
+    if debug: print(f"result:\t{result}")
     return result
 
 
